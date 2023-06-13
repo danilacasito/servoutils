45a0a9e1273e572fe653e79b7ac2fbcc13e0bdc2
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2023-06-13 18:52:34 +0000

    Add sqlite3, config and data.

---
+++ src/main/java/me/elordenador/basicbans/ConfigHelper.java
@@ -0,0 +1,16 @@
+package me.elordenador.basicbans;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import java.io.File;
+public class ConfigHelper {
+    Boolean mySQLEnabled;
+    String JDBCString;
+    public ConfigHelper(FileConfiguration config, File dataFolder) {
+        mySQLEnabled = config.getBoolean("MYSQL");
+        if (mySQLEnabled) {
+            this.JDBCString = config.getString("MYSQL_JDBC");
+        } else {
+            this.JDBCString = "jdbc:sqlite:"+dataFolder.getAbsolutePath()+config.getString("SQLITE_FILE");
+        }
+    }
+}
--- src/main/java/me/elordenador/basicbans/Init.java
+++ src/main/java/me/elordenador/basicbans/Init.java
@@ -1,15 +1,48 @@
 package me.elordenador.basicbans;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.logging.Logger;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Init extends JavaPlugin {
+    public Logger logger;
+    private FileConfiguration config;
     @Override
     public void onEnable() {
-        this.getLogger().info("Loading...");
+        this.logger = this.getLogger();
+        File configFile = new File(this.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            logger.log("config.yml doesn't exist, Creating one for you");
+
+            saveResource("config.yml", false);
+        }
+        configFile = new File(this.getDataFolder(), "config.yml");
+        this.config = YamlConfiguration.loadConfiguration(configFile);
+        ConfigHelper helper = new ConfigHelper(this.config, this.getDataFolder());
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(helpetr.JDBCString);
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos SQLite: " + e.getMessage());
+            this.getPluginLoader().disablePlugin(this);
+        }
+        
     }
 
     @Override
     public void onDisable() {
-        this.getLogger().info("Turning off...");
+        this.logger.info("Turning off...");
     }
 }
