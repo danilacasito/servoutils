6422cf63ff3a4e66a9403946571ddcde10dbae4c
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2023-06-13 19:15:15 +0000

    Fix variable declaration issue

--- src/main/java/me/elordenador/basicbans/DatabaseHelper.java
+++ src/main/java/me/elordenador/basicbans/DatabaseHelper.java
@@ -14,7 +14,7 @@ public class DatabaseHelper {
 
     public Boolean createTables() {
         try {
-            statement = connection.createStatement();
+            Statement statement = connection.createStatement();
             String createTableQuery = "CREATE TABLE IF NOT EXISTS sanciones (" +
                     "ID INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "Username TEXT," +
